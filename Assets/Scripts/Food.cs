fb1ecc12f1d926bae0a081fdc543bf1d5edf7bdd
Jeroen van Vroonhoven <nieterd@hotmail.com>
2015-03-14 14:48:52 +0100

    Food management added

---
+++ Assets/Scripts/Food.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System.Collections;
+
+public class Food {
+
+	public static Food COLA = new Food(5, 3);
+	public static Food STUFF = new Food(3, 2);
+
+
+	public int Duration, Increase;
+
+	private Food(int increase, int duration) {
+		this.Duration = duration;
+		this.Increase = increase;
+	}
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,12 +1,49 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Player : MonoBehaviour {
-
+	
 	public Tile currentTile;
+
+	private List<Food> eatenFood = new List<Food>();
+	private void EatFood(Food food) {
+		eatenFood.Add(food);
+	}
+
+	public int glucoseLevel = 0;
+
+	private void Nom() {
+		List<Food> toRemove = new List<Food>();
+
+		foreach(Food food in eatenFood) {
+			glucoseLevel += food.Increase;
+			food.Duration--;
+
+			if(food.Duration == 0) {
+				toRemove.Add(food);
+			}
+		}
+		foreach(Food food in toRemove) {
+			eatenFood.Remove(food);
+		}
+
+	}
 
 	// Use this for initialization
 	void Start () {
+
+		EatFood(Food.STUFF);
+		EatFood(Food.COLA);
+
+
+		Nom ();
+		Debug.Log(glucoseLevel);
+		EatFood (Food.COLA);
+		Nom ();
+		Debug.Log(glucoseLevel);
+
+		
 		
 	}
 	
