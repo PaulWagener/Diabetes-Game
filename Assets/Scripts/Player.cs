400a7c4d0e87c20e458c185063bc6c58c86e7c78
Paul Wagener <mail@paulwagener.nl>
2015-03-14 13:50:03 +0100

    Boardgame prototype

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public Tile currentTile;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.position = Vector3.Slerp (gameObject.transform.position, currentTile.transform.position, 0.1f);
+
+	}
+}
