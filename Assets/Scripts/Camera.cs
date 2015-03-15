0d41e3a56400bea51881642c78c53365d30fa818
Paul Wagener <mail@paulwagener.nl>
2015-03-15 14:52:29 +0100

    It's a game now

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera : MonoBehaviour {
+
+	public Game game;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 position = game.CurrentPlayer.gameObject.transform.position;
+		transform.position = Vector3.Lerp (transform.position, new Vector3 (position.x, position.y, -10.0f), 0.05f);
+	}
+}
