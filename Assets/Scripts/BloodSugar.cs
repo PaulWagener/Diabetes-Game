e8f3c8aac37afc967e0b8af0e2db9585c0f5dd27
Paul Wagener <mail@paulwagener.nl>
2015-03-15 11:23:05 +0100

    Added new bloodsugar meter

---
+++ Assets/Scripts/BloodSugar.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class BloodSugar : MonoBehaviour {
+
+	public Text levelText;
+	public RectTransform Indicator;
+
+	public Player linkedPlayer;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	public static float Map(float x, float in_min, float in_max, float out_min, float out_max) {
+		return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		levelText.text = "Bloedsuiker: " + linkedPlayer.glucoseLevel;
+		float x = Map (linkedPlayer.glucoseLevel, 0.0f, 15.0f, -208.0f, 208.0f);
+		Indicator.anchoredPosition = new Vector3 (x, Indicator.anchoredPosition.y);
+			//.position = new Vector3 (0.0f, Indicator.position.y, Indicator.position.y);
+	}
+}
